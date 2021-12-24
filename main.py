bdbb4f435e0fb6e825c1248956c1551607a68314
sakhasakhar <jenderwd@gmail.com>
2021-12-24 17:09:17 +0300

    graphics start

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import pygame
+import sys
+import os
 from math import ceil, floor
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Entity:  # сущность - думаю, можно или объединить в этом классе игрока и призраков, либо какие-то из них унаследовать от этого класса.
-    def __init__(self, pos, board):
+    def __init__(self, pos, board, name):
         self.board = board
         self.pos = pos
-        self.speed = 0.1  # от 0.1 до 1
+        self.name = name
+        self.speed = 0.1 # от 0.1 до 1
         self.dir1 = 0  # освновное направление
         self.dir2 = 0  # если игрок изменил направление тогда, когда в том направлении была стена, записывается сюда
-        self.directions = {0: (1, 0),    # вправо
-                           1: (-1, 0),   # влево
-                           2: (0, 1),    # вниз
-                           3: (0, -1)}   # вверх
+        self.directions = {0: ((1, 0), 'right'),    # вправо
+                           1: ((-1, 0), 'left'),   # влево
+                           2: ((0, 1), 'down'),    # вниз
+                           3: ((0, -1), 'up')}   # вверх
         
     def change_dir(self, dr):
         self.dir2 = dr
     
     def change_coords(self):
         for direction in [self.dir2, self.dir1]:  # сначала обрабатывает dir2, если не сработало - dir1
-            x, y = self.directions[direction]
+            x, y = self.directions[direction][0]
             x1 = round(self.pos[0] + x * self.speed, 1)   # изменяет координаты в соответствии с направлением
             y1 = round(self.pos[1] + y * self.speed, 1)
             try:
@@ -43,6 +63,10 @@ class Entity:  # сущность - думаю, можно или объедин
                 return
             except:
                 pass
+
+    def get_image(self):
+        im = self.name + self.directions[self.dir1][1] + '.png'
+        return load_image(im)
             
 
 class Board:
@@ -69,7 +93,7 @@ class Board:
         self.left = 50
         self.top = 50
         self.cell_size = 50
-        self.kush = Entity([1, 12], self)   # игрок
+        self.kush = Entity([1, 12], self, 'kush')   # игрок
 
     def render(self, screen):
         x, y = self.kush.pos
@@ -80,12 +104,12 @@ class Board:
             for j in range(self.height):
                 a = self.board[j][i]
                 rct = (*self.get_coords([i, j]), self.cell_size, self.cell_size)
-                pygame.draw.rect(screen, (255, 255, 255), rect=rct, width=1)
-                if a == 1:  # стены
-                    screen.fill((255, 0, 0), rect=rct)
+                # pygame.draw.rect(screen, (255, 255, 255), rect=rct, width=1)
+                # if a == 1:  # стены
+                #     screen.fill((255, 0, 0), rect=rct)
                 if a == 0:  # точки
                     screen.fill((255, 255, 0), rect=(rct[0] + self.cell_size // 2 - 5, rct[1] + self.cell_size // 2 - 5, 10, 10))
-        screen.fill((255, 255, 0), rect=(*self.get_coords(self.kush.pos), self.cell_size, self.cell_size))
+        screen.blit(self.kush.get_image(), (self.get_coords(self.kush.pos)))
         
             
     def get_coords(self, pos):   # преобразует позицию клетки в кординаты её левого верхнего угла
@@ -99,6 +123,7 @@ if True:
     pygame.display.set_caption("Kushats")
     size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
+    screen.blit(load_image('background.png'), (0, 0))
     board = Board(14, 14)
     clock = pygame.time.Clock()
     running = True
@@ -112,7 +137,7 @@ if True:
                 else:
                     print(event.key)
         clock.tick(120)
-        screen.fill((0, 0, 0))
+        screen.blit(load_image('background.png'), (0, 0))
         board.kush.change_coords()
         board.render(screen)
         pygame.display.flip()
