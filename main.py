061ea8d55139c99be17acb5a4c8ddce6af14478f
Sakha <jenderwd@gmail.com>
2022-10-14 20:34:57 +0300

    random directions for ghosts ver1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 import sqlite3
 from math import ceil, floor
-from random import randint
+from random import randint, choice
 
 directions = {0: ((1, 0), 'right'),  # вправо
               1: ((-1, 0), 'left'),  # влево
@@ -15,9 +15,10 @@ volume = 0.3  # громкость музыки
 
 difficulty = 2
 
-diffs = {0: (0.25, 0.0625),
-         1: (0.5, 0.1),
-         2: (0.5, 0.25)}
+# скорость кушаца, призраков, модель поведения мандарина и клауди
+diffs = {0: (0.25, 0.0625, 0),
+         1: (0.5, 0.1, 1),
+         2: (0.5, 0.25, 1)}
 
 
 def load_image(name, colorkey=None):
@@ -25,7 +26,8 @@ def load_image(name, colorkey=None):
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        fullname = os.path.join('images', 'noimage.png')
+        # sys.exit()
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -114,9 +116,10 @@ class Entity:  # сущность - игрок и призраки
         if a[2]:
             self.pos = (a[0], a[1])
             self.dir1 = self.dir2
-            return
+            return True
         a = self.can_move(self.dir1)
         self.pos = (a[0], a[1])
+        return a[2]
 
     def get_image(self):
         # для картинок из таймаута
@@ -153,18 +156,34 @@ class Ghost(Entity):
         if not self.check_state():
             # призраки не двигаются в режиме таймаута
             return
-        if self.trajectory[self.point] == self.pos:
-            self.point = (self.point + 1) % len(self.trajectory)
-        x = self.trajectory[self.point][0] - self.pos[0]
-        y = self.trajectory[self.point][1] - self.pos[1]
-        if x != 0:
-            x = abs(x) / x
-        if y != 0:
-            y = abs(y) / y
-        self.dir1 = (x, y)
-        x1 = round(self.pos[0] + x * self.speed, 4)
-        y1 = round(self.pos[1] + y * self.speed, 4)
-        self.pos = x1, y1
+        if self.trajectory:
+            if self.trajectory[self.point] == self.pos:
+                self.point = (self.point + 1) % len(self.trajectory)
+            x = self.trajectory[self.point][0] - self.pos[0]
+            y = self.trajectory[self.point][1] - self.pos[1]
+            if x != 0:
+                x = abs(x) / x
+            if y != 0:
+                y = abs(y) / y
+            self.dir1 = (x, y)
+            x1 = round(self.pos[0] + x * self.speed, 4)
+            y1 = round(self.pos[1] + y * self.speed, 4)
+            self.pos = x1, y1
+        else:
+            if not self.change_coords():
+                d1 = (self.dir2[1], self.dir2[0])
+                d2 = (-self.dir2[1], -self.dir2[0])
+                if self.can_move(d1):
+                    if self.can_move(d2):
+                        d = choice((d1, d2))
+                    else:
+                        d = d1
+                elif self.can_move(d2):
+                    d = d2
+                else:
+                    d = (-self.dir2[0], self.dir2[1])
+                    print(self.dir2, d1, d2, d)
+                self.dir2 = d
 
     def check_kush(self):
         # если кушац пересекается с призраком, то оба входят в режим таймаута - меняют спрайт
@@ -252,20 +271,24 @@ class Board:
             sweet = Sweet(self, name)
             self.sweets.append(sweet)
         chaser = Chaser([12, 3], self, speed=difsets[1])  # привидение которое движется к игроку
-        cloudy = Ghost((1, 0), self, [(3, 0), (3, 9), (4, 9), (4, 11),
-                                      (5, 11), (5, 13), (3, 13), (3, 11),
-                                      (6, 11), (6, 12), (7, 12), (7, 13),
-                                      (9, 13), (9, 9), (13, 9),
-                                      (13, 12), (12, 12), (12, 13), (10, 13),
-                                      (10, 12), (9, 12), (9, 10), (6, 10),
-                                      (6, 11), (4, 11), (4, 9), (3, 9),
-                                      (3, 2), (1, 2), (1, 0)], name='cloudy', speed=difsets[1])
-        mandarin = Ghost((10, 2), self, [(10, 0), (12, 0), (12, 1), (13, 1),
-                                         (13, 3), (6, 3), (9, 3), (9, 0),
-                                         (7, 0), (7, 1), (5, 1), (5, 2),
-                                         (0, 2), (0, 1), (1, 1), (1, 0), (3, 0),
-                                         (3, 3), (4, 3), (4, 2), (5, 2),
-                                         (5, 0), (9, 0), (9, 2), (10, 2)], name='mandarin', speed=difsets[1])
+        if not difsets[2]:
+            cloudy = Ghost((1, 0), self, [(3, 0), (3, 9), (4, 9), (4, 11),
+                                          (5, 11), (5, 13), (3, 13), (3, 11),
+                                          (6, 11), (6, 12), (7, 12), (7, 13),
+                                          (9, 13), (9, 9), (13, 9),
+                                          (13, 12), (12, 12), (12, 13), (10, 13),
+                                          (10, 12), (9, 12), (9, 10), (6, 10),
+                                          (6, 11), (4, 11), (4, 9), (3, 9),
+                                          (3, 2), (1, 2), (1, 0)], name='cloudy', speed=difsets[1])
+            mandarin = Ghost((10, 2), self, [(10, 0), (12, 0), (12, 1), (13, 1),
+                                             (13, 3), (6, 3), (9, 3), (9, 0),
+                                             (7, 0), (7, 1), (5, 1), (5, 2),
+                                             (0, 2), (0, 1), (1, 1), (1, 0), (3, 0),
+                                             (3, 3), (4, 3), (4, 2), (5, 2),
+                                             (5, 0), (9, 0), (9, 2), (10, 2)], name='mandarin', speed=difsets[1])
+        else:
+            cloudy = Ghost((1, 0), self, None, name='cloudy', speed=difsets[1])
+            mandarin = Ghost((10, 2), self, None, name='mandarin', speed=difsets[1])
         self.ghosts = [chaser, cloudy, mandarin]
         self.score = 0
         self.ghost_sound = pygame.mixer.Sound('sounds/ghost attack.mp3')
